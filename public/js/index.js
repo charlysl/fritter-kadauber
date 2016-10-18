5cc0c8be03d8d621afcf36e9230acd12bfcc8631
Kimberly Dauber <kadauber@mit.edu>
2016-10-18 19:51:52 -0400

    implemented duplicate username error message

--- public/js/index.js
+++ public/js/index.js
@@ -6,6 +6,19 @@ $(document).ready(function() {
 
   var navbarController = NavbarController();
 
+  // Keep track of form group elements
+  var formGroups = [ $("#username-group"), $("#password-group"), // modal login
+    $("#username-register-group"), $("#password-register-group"), $("#password-check-register-group"), // modal register
+    $("#username-prompt-group"), $("#password-prompt-group"), // prompt login
+    $("#username-prompt-register-group"), $("#password-prompt-register-group"), $("#password-prompt-check-register-group") // prompt register
+  ];
+
+  // Keep track of error message elements
+  var errorMessages = [ $("#invalid-login-message"), // modal login
+    $("#non-unique-user-message"), $("#non-matching-passwords-message"), // modal register
+    $("#invalid-login-prompt-message"), // prompt login
+    $("#non-unique-user-prompt-message"), $("#non-matching-passwords-prompt-message") // prompt register
+  ];
 
   // Function to be called when rendering the navbar
   var renderNavbar = function () {
@@ -16,27 +29,32 @@ $(document).ready(function() {
 
       // The function to call when a user asks to set their username
       var setUsername = function () {
+        console.log("setting username");
         // Retrieve the new username from the username setting modal input
-        var newUsername = $("#username-input").val();
-        var newPasswordHash = $("#password-input").val();
+        var newUsername = $("#username-register-input").val();
+        var newPasswordHash = $("#password-register-input").val();
         // Send a post request to username altering the username stored
         // for the session
         $.post('/fritter/register', {
             username: newUsername,
             passwordHash: newPasswordHash
         }, function (res) {
+          console.log("handling response", res.success);
           if (res.success) {
             updateWholePage();
           } else {
-            alert(res.message);
+            $("#username-register-group").addClass("has-error");
+            $("#non-unique-user-message").show();
           }
         });
       }
 
       navbarController.registerUsernameUpdater(setUsername);
 
-      navbarController.attachUsernameUpdaterListener("username-input");
-      navbarController.attachUsernameUpdaterListener("set-username-btn");
+      navbarController.attachUsernameUpdaterListener("username-register-input");
+      navbarController.attachUsernameUpdaterListener("password-register-input");
+      navbarController.attachUsernameUpdaterListener("password-check-register-input");
+      navbarController.attachUsernameUpdaterListener("register-btn");
     });
   }
 
@@ -119,6 +137,22 @@ $(document).ready(function() {
     $("#change-username-modal").modal("hide"); // make sure modals are totally hidden
     $('body').removeClass('modal-open');
     $('.modal-backdrop').remove();
+    
+    // No errors should be showing after a page update
+    hideErrors();
+  }
+
+  // A helper function for hiding all the errors on the page.
+  var hideErrors = function () {
+    // Hide all error messages
+    errorMessages.forEach(function (message) {
+      message.hide();
+    });
+
+    // Input elements should not look like they have errors
+    inputs.forEach(function (input) {
+      input.removeClass("has-error");
+    });
   }
   
 
